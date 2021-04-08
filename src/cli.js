22985ed7422ff765e90e37447836c7fde1db0ef0
Natalia <y_natalya_v@mail.ru>
2021-04-08 22:14:34 +0300

    l4 - fix-2

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
 import readlineSync from 'readline-sync';
 
-    const name = readlineSync.question('May I have your name? ');
-    export default name;
+  const name = readlineSync.question('May I have your name? ');
+  export default name;
